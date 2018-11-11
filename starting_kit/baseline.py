809a9ff6cfc0e65159424568a866f3e7e2fe9976
AyushRai09 <ayushrai982@gmail.com>
2018-11-11 18:47:02 +0530

    Siamese network model implemented in the buildModel function of the code

--- starting_kit/baseline.py
+++ starting_kit/baseline.py
@@ -4,9 +4,12 @@ from keras.preprocessing.text import Tokenizer
 from keras.preprocessing.sequence import pad_sequences
 from keras.utils import to_categorical
 from keras.models import Sequential
-from keras.layers import Dense, Embedding, LSTM
+from keras.layers import Dropout, Dense, Embedding, LSTM, Conv1D, MaxPooling1D, Activation, Bidirectional, Input
 from keras import optimizers
+from keras.layers.merge import concatenate
+from keras.layers.normalization import BatchNormalization
 from keras.models import load_model
+from keras.models import Model
 import json, argparse, os
 import re
 import io
@@ -216,10 +219,39 @@ def buildModel(embeddingMatrix):
                                 weights=[embeddingMatrix],
                                 input_length=MAX_SEQUENCE_LENGTH,
                                 trainable=False)
-    model = Sequential()
-    model.add(embeddingLayer)
-    model.add(LSTM(LSTM_DIM, dropout=DROPOUT))
-    model.add(Dense(NUM_CLASSES, activation='sigmoid'))
+    lstm_layer = Bidirectional(LSTM(LSTM_DIM))
+    # Creating LSTM Encoder layer for First Sentence
+    sequence_1_input = Input(shape=(MAX_SEQUENCE_LENGTH,), dtype='int32')
+    embedded_sequences_1 = embeddingLayer(sequence_1_input)
+    x1 = lstm_layer(embedded_sequences_1)
+
+    # Creating LSTM Encoder layer for Second Sentence
+    sequence_2_input = Input(shape=(MAX_SEQUENCE_LENGTH,), dtype='int32')
+    embedded_sequences_2 = embeddingLayer(sequence_2_input)
+    x2 = lstm_layer(embedded_sequences_2)
+
+    # Creating leaks input
+#     leaks_input = Input(shape=(leaks_train.shape[1],))
+#     leaks_dense = Dense(self.number_dense_units/2, activation=self.activation_function)(leaks_input)
+
+    # Merging two LSTM encodes vectors from sentences to
+    # pass it to dense layer applying dropout and batch normalisation
+    merged = concatenate([x1, x2])
+    merged = BatchNormalization()(merged)
+    merged = Dropout(0.1)(merged)
+    merged = Dense(50, activation='sigmoid')(merged)
+    merged = BatchNormalization()(merged)
+    merged = Dropout(0.1)(merged)
+    preds = Dense(NUM_CLASSES, activation='sigmoid')(merged)
+    model = Model(inputs=[sequence_1_input, sequence_2_input], outputs=preds)
+#     model = Sequential()
+#     model.add(embeddingLayer)
+#     model.add(LSTM(LSTM_DIM, dropout=DROPOUT))
+#     model.add(Dropout(0.2))
+#     model.add(Conv1D(64, 5, activation='relu'))
+#     model.add(MaxPooling1D(pool_size=4))
+#     model.add(LSTM(LSTM_DIM))
+#     model.add(Dense(NUM_CLASSES, activation='sigmoid'))
     
     rmsprop = optimizers.rmsprop(lr=LEARNING_RATE)
     model.compile(loss='categorical_crossentropy',
@@ -256,30 +288,30 @@ def main():
     LEARNING_RATE = config["learning_rate"]
     NUM_EPOCHS = config["num_epochs"]
         
-    print("Processing training data...")
+    print "Processing training data..."
     trainIndices, trainTexts, labels = preprocessData(trainDataPath, mode="train")
     # Write normalised text to file to check if normalisation works. Disabled now. Uncomment following line to enable   
     # writeNormalisedData(trainDataPath, trainTexts)
-    print("Processing test data...")
+    print "Processing test data..."
     testIndices, testTexts = preprocessData(testDataPath, mode="test")
     # writeNormalisedData(testDataPath, testTexts)
 
-    print("Extracting tokens...")
+    print "Extracting tokens..."
     tokenizer = Tokenizer(num_words=MAX_NB_WORDS)
     tokenizer.fit_on_texts(trainTexts)
     trainSequences = tokenizer.texts_to_sequences(trainTexts)
     testSequences = tokenizer.texts_to_sequences(testTexts)
 
     wordIndex = tokenizer.word_index
-    print("Found %s unique tokens." % len(wordIndex))
+    print "Found %s unique tokens." % len(wordIndex)
 
-    print("Populating embedding matrix...")
+    print"Populating embedding matrix..."
     embeddingMatrix = getEmbeddingMatrix(wordIndex)
 
     data = pad_sequences(trainSequences, maxlen=MAX_SEQUENCE_LENGTH)
     labels = to_categorical(np.asarray(labels))
-    print("Shape of training data tensor: ", data.shape)
-    print("Shape of label tensor: ", labels.shape)
+    print "Shape of training data tensor: ", data.shape
+    print "Shape of label tensor: ", labels.shape
         
     # Randomize data
     np.random.shuffle(trainIndices)
@@ -292,60 +324,60 @@ def main():
                "microRecall" : [],
                "microF1" : []}
     
-    print("Starting k-fold cross validation...")
-    for k in range(NUM_FOLDS):
-        print('-'*40)
-        print("Fold %d/%d" % (k+1, NUM_FOLDS))
-        validationSize = int(len(data)/NUM_FOLDS)
-        index1 = validationSize * k
-        index2 = validationSize * (k+1)
+    print "Starting k-fold cross validation..."
+#     for k in range(NUM_FOLDS):
+#         print '-'*40
+#         print "Fold %d/%d" % (k+1, NUM_FOLDS)
+#         validationSize = int(len(data)/NUM_FOLDS)
+#         index1 = validationSize * k
+#         index2 = validationSize * (k+1)
             
-        xTrain = np.vstack((data[:index1],data[index2:]))
-        yTrain = np.vstack((labels[:index1],labels[index2:]))
-        xVal = data[index1:index2]
-        yVal = labels[index1:index2]
-        print("Building model...")
-        model = buildModel(embeddingMatrix)
-        model.fit(xTrain, yTrain, 
-                  validation_data=(xVal, yVal),
-                  epochs=NUM_EPOCHS, batch_size=BATCH_SIZE)
+#         xTrain = np.vstack((data[:index1],data[index2:]))
+#         yTrain = np.vstack((labels[:index1],labels[index2:]))
+#         xVal = data[index1:index2]
+#         yVal = labels[index1:index2]
+#         print "Building model..."
+#         print(xTrain.shape, yTrain.shape)
+#         model = buildModel(embeddingMatrix)
+#         model.fit([xTrain,xTrain], yTrain, 
+#                   validation_data=([xVal,xVal], yVal),
+#                   epochs=NUM_EPOCHS, batch_size=BATCH_SIZE)
 
-        predictions = model.predict(xVal, batch_size=BATCH_SIZE)
-        accuracy, microPrecision, microRecall, microF1 = getMetrics(predictions, yVal)
-        metrics["accuracy"].append(accuracy)
-        metrics["microPrecision"].append(microPrecision)
-        metrics["microRecall"].append(microRecall)
-        metrics["microF1"].append(microF1)
+#         predictions = model.predict([xVal,xVal], batch_size=BATCH_SIZE)
+#         accuracy, microPrecision, microRecall, microF1 = getMetrics(predictions, yVal)
+#         metrics["accuracy"].append(accuracy)
+#         metrics["microPrecision"].append(microPrecision)
+#         metrics["microRecall"].append(microRecall)
+#         metrics["microF1"].append(microF1)
         
-    print("\n============= Metrics =================")
-    print("Average Cross-Validation Accuracy : %.4f" % (sum(metrics["accuracy"])/len(metrics["accuracy"])))
-    print("Average Cross-Validation Micro Precision : %.4f" % (sum(metrics["microPrecision"])/len(metrics["microPrecision"])))
-    print("Average Cross-Validation Micro Recall : %.4f" % (sum(metrics["microRecall"])/len(metrics["microRecall"])))
-    print("Average Cross-Validation Micro F1 : %.4f" % (sum(metrics["microF1"])/len(metrics["microF1"])))
+#     print "\n============= Metrics ================="
+#     print "Average Cross-Validation Accuracy : %.4f" % (sum(metrics["accuracy"])/len(metrics["accuracy"]))
+#     print "Average Cross-Validation Micro Precision : %.4f" % (sum(metrics["microPrecision"])/len(metrics["microPrecision"]))
+#     print "Average Cross-Validation Micro Recall : %.4f" % (sum(metrics["microRecall"])/len(metrics["microRecall"]))
+#     print "Average Cross-Validation Micro F1 : %.4f" % (sum(metrics["microF1"])/len(metrics["microF1"]))
     
-    print("\n======================================")
+#     print "\n======================================"
     
-    print("Retraining model on entire data to create solution file")
-    model = buildModel(embeddingMatrix)
-    model.fit(data, labels, epochs=NUM_EPOCHS, batch_size=BATCH_SIZE)
-    model.save('EP%d_LR%de-5_LDim%d_BS%d.h5'%(NUM_EPOCHS, int(LEARNING_RATE*(10**5)), LSTM_DIM, BATCH_SIZE))
-    # model = load_model('EP%d_LR%de-5_LDim%d_BS%d.h5'%(NUM_EPOCHS, int(LEARNING_RATE*(10**5)), LSTM_DIM, BATCH_SIZE))
+#     print "Retraining model on entire data to create solution file"
+#     model = buildModel(embeddingMatrix)
+#     model.fit([data,data], labels, epochs=NUM_EPOCHS, batch_size=BATCH_SIZE)
+#     model.save('EP%d_LR%de-5_LDim%d_BS%d.h5_SiameseNetwork'%(NUM_EPOCHS, int(LEARNING_RATE*(10**5)), LSTM_DIM, BATCH_SIZE))
+    model = load_model('EP%d_LR%de-5_LDim%d_BS%d.h5_SiameseNetwork'%(NUM_EPOCHS, int(LEARNING_RATE*(10**5)), LSTM_DIM, BATCH_SIZE))
 
-    print("Creating solution file...")
+    print "Creating solution file..."
     testData = pad_sequences(testSequences, maxlen=MAX_SEQUENCE_LENGTH)
-    predictions = model.predict(testData, batch_size=BATCH_SIZE)
+    predictions = model.predict([testData,testData], batch_size=BATCH_SIZE)
     predictions = predictions.argmax(axis=1)
 
     with io.open(solutionPath, "w", encoding="utf8") as fout:
-        fout.write('\t'.join(["id", "turn1", "turn2", "turn3", "label"]) + '\n')        
+        fout.write(unicode('\t'.join(["id", "turn1", "turn2", "turn3", "label"]) + '\n',"utf-8"))        
         with io.open(testDataPath, encoding="utf8") as fin:
             fin.readline()
             for lineNum, line in enumerate(fin):
                 fout.write('\t'.join(line.strip().split('\t')[:4]) + '\t')
-                fout.write(label2emotion[predictions[lineNum]] + '\n')
-    print("Completed. Model parameters: ")
-    print("Learning rate : %.3f, LSTM Dim : %d, Dropout : %.3f, Batch_size : %d" 
-          % (LEARNING_RATE, LSTM_DIM, DROPOUT, BATCH_SIZE))
+                fout.write(unicode(label2emotion[predictions[lineNum]] + '\n',"utf-8"))
+    print "Completed. Model parameters: "
+    print "Learning rate : %.3f, LSTM Dim : %d, Dropout : %.3f, Batch_size : %d" % (LEARNING_RATE, LSTM_DIM, DROPOUT, BATCH_SIZE)
     
                
 if __name__ == '__main__':
